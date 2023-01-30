c647b9c3be725ab406ae2cbe96bc3f5a2c5b8cfe
Anna Barton <anna.barton327@gmail.com>
2023-01-30 16:27:07 -0700

    commit tree 0fff054b2b350e5e61ffb8212dbcf184f5f3bcf9

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return "This one is on me!" 
+  }
+   if (2500 > feet && feet > 2000) {
+    return "I will gladly take your thirty bucks."
+  }
+   if (feet > 2500) {
+    return "No can do."
+  }
+   else if ( 40 > feet && feet < 2000)
+    return "That will be twenty bucks."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  const city = "NYC"
+  return city ? "Ok, sounds good." : "No go."  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case "generous":
+      return 'Thank you so much.'
+    case "not as generous":
+      return "Thank you."
+  }
 }
